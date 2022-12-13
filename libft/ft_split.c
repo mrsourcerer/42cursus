da8d6052c4a9f8e9806eaf71973dc3e1ffd2cdf0
sourcerer <mr.sourcerer@gmail.com>
2022-12-13 07:30:24 +0100

    2022.12.13 home early

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 11:02:56 by danlopez          #+#    #+#             */
-/*   Updated: 2022/12/12 07:25:40 by danlopez         ###   ########.fr       */
+/*   Updated: 2022/12/13 07:22:39 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,23 @@ size_t	ft_words(char const *s, char c)
 	size_t	count;
 
 	count = 0;
-	while (*s == c)
+	while (*s == c && *s)
 		s++;
 	while (*s)
 	{
-		if (*s == c)
+		if (*s != c)
 			{
 				count++;
-				while (*s == c)
+				while (*s != c)
 					s++;
 			}
-		s++;
+		else
+			s++;
 	}
-	return (count + 1);
+//	if (count == 0)
+//		return (0);
+//	return (count + 1);
+	return (count);
 }
 
 size_t	ft_next(char const *s, char c, size_t start)
@@ -37,8 +41,12 @@ size_t	ft_next(char const *s, char c, size_t start)
 	size_t	size;
 
 	size = 0;
+	s = s + start;
 	while (*s == c)
+	{
 		s++;
+		size++;
+	}
 	while (*s)
 	{
 		if (*s == c)
@@ -48,12 +56,20 @@ size_t	ft_next(char const *s, char c, size_t start)
 	}
 	return (size);
 }
-void	ft_add(char const *s, char c, char **dict, size_t start, size_t size)
+char	*ft_add(char const *s, char c, size_t start, size_t size)
 {
+	char	*word;
+	s = s + start;
 	while (*s == c)
+	{
 		s++;
-	///////////////////////////////////////// voy aqui, malloc y dar valor
-
+		size--;
+	}
+	word = (char *)malloc((size + 1) * sizeof(char));
+	if (!word)
+		return (0);
+	word = ft_substr(s, 0, size);
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
@@ -62,18 +78,20 @@ char	**ft_split(char const *s, char c)
 	char	**dict;
 	size_t	start;
 	size_t	size;
+	size_t	i;
 
 	count = ft_words(s, c);
 	dict = (char **)malloc((count + 1) * sizeof(size_t));
 	if (!dict)
 		return (0);
 	start = 0;
-	while (count > 0)
+	i = 0;
+	while (i < count)
 	{
 		size = ft_next(s, c, start);
-		ft_add(s, c, dict, start, size);
-		start = size + 1;
-		count--;
+		dict[i] = ft_add(s, c, start, size);
+		start = start + size + 1;
+		i++;
 	}
 /*	if (count == 1)
 	{
