58ab34db093ea07e603b1bccdac78365b9a57071
dani <danlopez@student.42urduliz.com>
2022-12-17 14:34:32 +0100

    2022.12.17 42

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,42 +6,73 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 06:13:24 by danlopez          #+#    #+#             */
-/*   Updated: 2022/12/16 07:30:15 by danlopez         ###   ########.fr       */
+/*   Updated: 2022/12/17 10:19:30 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putn(long n, int fd)
+size_t	ft_intsize_nomalloc(long n)
 {
-	if (n > 9)
+	size_t	size;
+
+	size = 0;
+	if (n < 0)
 	{
-		ft_putn(n / 10, fd);
-		ft_putchar_fd((int)(n % 10) + '0', fd);
+		size++;
+		n = n * -1;
 	}
-	else
-		ft_putchar_fd((int)n + '0', fd);
+	while (n > 9)
+	{
+		size++;
+		n = n / 10;
+	}
+	size++;
+	return (size);
+}
+
+int	ft_pow_nomalloc(int n)
+{
+	int	i;
+
+	i = 1;
+	while (n > 0)
+	{
+		i = i * 10;
+		n--;
+	}
+	return (i);
+}
+
+char	*ft_itoa_nomalloc(int n)
+{
+	long		num;
+	size_t		size;
+	size_t		i;
+	static char	str[12];
+	char		*num_str;
+
+	num = n;
+	size = ft_intsize_nomalloc(num);
+	num_str = str;
+	i = 0;
+	if (num < 0)
+	{
+		num_str[i] = '-';
+		num = num * -1;
+		i++;
+	}
+	while (i < size)
+	{
+		num_str[i] = (num / ft_pow_nomalloc(size - i - 1)) + '0';
+		num = num % ft_pow_nomalloc(size - i - 1);
+		i++;
+	}
+	num_str[size] = '\0';
+	return (str);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	num;
-
-	num = n;
-	if (num < 0)
-	{
-		num = num * -1;
-		ft_putchar_fd('-', fd);
-	}
-	while(num > 9)
-	{
-		ft_putn(num, fd);
-	}
-
-/*	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
-	free(str);
-	*/
+	ft_putstr_fd(ft_itoa_nomalloc(n), fd);
 }
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,12 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 11:02:56 by danlopez          #+#    #+#             */
-/*   Updated: 2022/12/14 06:39:00 by danlopez         ###   ########.fr       */
+/*   Updated: 2022/12/17 14:18:09 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h> //////////////////////////////////////////////////////////////
 
 size_t	ft_words(char const *s, char c)
 {
@@ -39,19 +40,34 @@ size_t	ft_next(char const *s, char c, size_t start)
 
 	size = 0;
 	s = s + start;
+	//printf("size prev: %zu\n", size);
 	while (*s == c)
 	{
 		s++;
 		size++;
 	}
+	//printf("size antes while: %zu\n", size);
 	while (*s)
 	{
 		if (*s == c)
 			return (size);
 		size++;
 		s++;
+		//printf("size en while: %zu\n", size);
 	}
+	//printf("size si no encuentra: %zu\n", size);
 	return (size);
+}
+
+char	**ft_free(size_t i, char **dict)
+{
+	i--;
+	while (i > 0)
+	{
+		free(dict[i]);
+		i--;
+	}
+	return (0);
 }
 
 char	*ft_add(char const *s, char c, size_t start, size_t size)
@@ -63,6 +79,7 @@ char	*ft_add(char const *s, char c, size_t start, size_t size)
 	{
 		s++;
 		size--;
+		//printf("resta un size\n");
 	}
 	word = (char *)malloc((size + 1) * sizeof(char));
 	if (!word)
@@ -80,6 +97,7 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 
 	count = ft_words(s, c);
+	//printf("cant words: %zu\n", count);
 	dict = (char **)malloc((count + 1) * sizeof(size_t));
 	if (!dict)
 		return (0);
@@ -88,7 +106,14 @@ char	**ft_split(char const *s, char c)
 	while (i < count)
 	{
 		size = ft_next(s, c, start);
+		//printf("palabra: %zu | size: %zu\n", i, size);
 		dict[i] = ft_add(s, c, start, size);
+		if (!dict[i])
+		{
+			ft_free(i, dict);
+			free(dict[0]);
+			return (0);
+		}
 		start = start + size + 1;
 		i++;
 	}
