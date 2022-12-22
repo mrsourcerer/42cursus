3f24b29ffba993e59ee5c751e7529884d78e5878
sourcerer <mr.sourcerer@gmail.com>
2022-12-22 07:29:41 +0100

    2022.12.22 home early

--- get_next_line_no42git/get_next_line.c
+++ get_next_line_no42git/get_next_line.c
@@ -6,22 +6,40 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 06:23:15 by danlopez          #+#    #+#             */
-/*   Updated: 2022/12/21 07:21:06 by danlopez         ###   ########.fr       */
+/*   Updated: 2022/12/22 07:28:52 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_check_end(char *str)
+{
+	char	*nl;
+
+	nl = ft_strchr(str, '\n');
+	if (*nl)
+		return (ft_strlen(str) - ft_strlen(nl));
+	return (-1);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buffer;
 	static char	*str;
+	char 		*line;
 
-
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (0);
-	if (!read(fd, buffer, BUFFER_SIZE))
-		return (0);
-	return (buffer);
+	if (!str)
+	{
+		buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buffer)
+			return (0);
+		if (!read(fd, buffer, BUFFER_SIZE))
+			return (0);
+		str = ft_strdup(buffer);
+		free(buffer);
+	}
+	if (ft_check_end(str) >= 0)
+		line = ft_substr(str, 0, ft_check_end(str));
+		//str = lo que queda menos line 
+	return (line);
 }
--- get_next_line_no42git/get_next_line_utils.c
+++ get_next_line_no42git/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 06:27:23 by danlopez          #+#    #+#             */
-/*   Updated: 2022/12/21 07:21:01 by danlopez         ###   ########.fr       */
+/*   Updated: 2022/12/22 07:20:17 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@ size_t	ft_strlen(const char *s)
 	while (s[len])
 		len++;
 	return (len);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*s;
+	char	*d;
+	size_t	i;
+
+	d = (char *)dst;
+	s = (char *)src;
+	i = 0;
+	if (!src && !dst)
+		return (dst);
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
@@ -62,4 +81,51 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		return (len_src + size);
 	else
 		return (len_src);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+
+	dst = (char *)malloc((ft_strlen(s1) + 1) * 1);
+	if (!dst)
+		return (0);
+	ft_strlcpy(dst, s1, (size_t)ft_strlen(s1) + 1);
+	return (dst);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	size;
+
+	size = len;
+	if (len + start > ft_strlen(s))
+	{
+		if (start > ft_strlen(s))
+			size = 0;
+		else
+			size = ft_strlen(s) - start;
+	}
+	sub = (char *)malloc((size + 1) * 1);
+	if (!sub)
+		return (0);
+	if (start > ft_strlen(s))
+		sub[0] = '\0';
+	else
+		ft_strlcpy(sub, s + start, size + 1);
+	return (sub);
 }
