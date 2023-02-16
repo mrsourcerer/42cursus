bd409ef888c8e45860b44cac3436ccdb72023e05
dani <danlopez@student.42urduliz.com>
2023-02-16 22:49:46 +0100

    2023.02.16 22:49 42 minor tests in ft_printf

--- prueba/printf_clion/main.c
+++ prueba/printf_clion/main.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 08:56:52 by danlopez          #+#    #+#             */
-/*   Updated: 2023/02/13 22:41:01 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/02/16 22:47:41 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,11 +138,11 @@ int	main(void)
 
 	printf("variado buscando errores...\n");
 	printf("Custom:   *");
-	size = ft_printf("%x", 16);
+	size = ft_printf("%%%");
 	printf("* fin custom\n");
 	printf("Escrito: %i\n", size);
 	printf("Original: *");
-	size = printf("%x", 16);
+	size = printf("%%");
 	printf("* fin original\n");
 	printf("Escrito: %i\n\n", size);
 
