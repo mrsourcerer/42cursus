f6f02ddf68bde16e92ed8f2f2a89b93c17449b84
sourcerer <mr.sourcerer@gmail.com>
2023-02-20 07:28:13 +0100

    2023.02.20 07:27h home

--- prueba/minitalk_clion/main.c
+++ prueba/minitalk_clion/main.c
@@ -1,6 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danlopez <danlopez@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 07:26:51 by danlopez          #+#    #+#             */
+/*   Updated: 2023/02/20 07:26:51 by danlopez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int main() {
-	printf("Hello, World!\n");
-	return 0;
+int	main(void)
+{
+	char	*tmp;
+	int		len;
+
+	tmp = "Hello, World!\n";
+	len = ft_len(tmp);
+	printf("%s %i", tmp, len);
+	return (0);
 }
