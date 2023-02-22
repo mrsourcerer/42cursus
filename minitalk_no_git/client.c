43f1575a17cd18923dfca6a1848ae8b3c3b3db67
sourcerer <mr.sourcerer@gmail.com>
2023-02-22 07:25:02 +0100

    2023.02.22 07:24h home messing with minitalk

--- minitalk_no_git/client.c
+++ minitalk_no_git/client.c
@@ -6,14 +6,31 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 12:33:14 by danlopez          #+#    #+#             */
-/*   Updated: 2023/02/18 13:15:22 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/02/22 07:24:01 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <signal.h>
+#include <stdio.h> // quitar en cuanto presente printf y lo integre ***********************
 
-int	main(int argc, char *argv)
+void	ft_send_str(int pid, int sig_num, char *str)
 {
+	return ;
+}
+
+int	main(int argc, char *argv[])
+{
+	int		pid;
+	char	*str;
+
 	if (argc < 3)
-		return ;
+		return (1);
+	pid = ft_atoi(argv[1]);
+	str = argv[2];
+	printf("Num: %i\n", pid);
+	printf("str: %s\n", str);
+	kill(pid, SIGUSR1);
+	ft_send_str(pid, SIGUSR1, str);
+	return (0);
 }
