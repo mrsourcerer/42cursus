25fb6c2d1b298aa02f3114dc1c740a7b4be1bca4
sourcerer <mr.sourcerer@gmail.com>
2023-03-08 07:26:56 +0100

    2023.03.08 07:25h home push_swap list almost ok

--- push_swap_no_git/push_swap.c
+++ push_swap_no_git/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 20:05:16 by danlopez          #+#    #+#             */
-/*   Updated: 2023/03/07 07:26:53 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/03/08 07:22:56 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,13 @@ static int	ft_duplicate(int *a, int argc)
 }
 */
 
-static int	ft_to_stack(char *argv[], t_list **a)
+static int	ft_to_int(char *argv[], int **values)
 {
 	int		i;
 	char	*p;
 	long	tmp;
-	int		i_tmp;
-	t_list	*new;
+	//int		i_tmp;
+	//t_list	*new;
 
 	i = 1;
 	while (argv[i])
@@ -59,13 +59,27 @@ static int	ft_to_stack(char *argv[], t_list **a)
 				p++;
 			}
 		}
-		i_tmp = (int)tmp;
-		new = ft_lstnew(&i_tmp);
-		ft_lstadd_front(a, new);
+		*(values[i - 1]) = (int)tmp; //da segmentation fault, corregir
+		//new = ft_lstnew(&i_tmp);
+		//ft_lstadd_front(a, new);
 		i++;
 	}
+	values[i - 1] = NULL;
 	return (0);
 	//return (ft_duplicate(*a, --i));
+}
+
+int	ft_value_to_list(int *values, t_list **a)
+{
+	t_list	*new;
+
+	while (values)
+	{
+		new = ft_lstnew(values);
+		ft_lstadd_back(a, new);
+		values++;
+	}
+	return (0);
 }
 
 /*
@@ -86,29 +100,29 @@ int	ft_sorted_int(int *a, int len)
 int	main(int argc, char *argv[])
 {
 	t_list	*a;
-	t_list	*tmp;
+	int 	*values;
 	//t_list	*b;
 	int		i;
 
 	if (argc < 2)
 		return (ft_printf("Error\n"), -1);
-	//a = (int *)malloc((argc) * sizeof(int));
+	values = (int *)malloc((argc) * sizeof(int));
 	//b = (int *)malloc((argc) * sizeof(int));
 //	if (!a || !b)
 //		return (ft_frint(&a), ft_frint(&b), -1);
-	if (ft_to_stack(argv, &a))
-		return (-1);
+	if (ft_to_int(argv, &values))
+		return (ft_frint(&values), -1);
 //		return (ft_frint(&a), ft_frint(&b), ft_printf("Error\n"), -1);
 //	if (ft_sort_min(a, b, argc) == -1)
 //		return (ft_frint(&a), ft_frint(&b), ft_printf("Error\n"), -1);
-
+	if (ft_value_to_list(values, &a))
+		return (ft_frint(&values), -1); // hay que free la lista
 	i = 0;
-	tmp = a;
-	while (tmp != NULL)
+	while (a != NULL)
 	{
-		ft_printf("i: %i - Content: %i - ", i, *(int *)(tmp->content));
-		ft_printf("Next Pointer: %p\n", tmp->next);
-		tmp = tmp->content;
+		ft_printf("i: %i - Content: %i - ", i, *(int *)(a->content));
+		ft_printf("Next Pointer: %p\n", a->next);
+		a = a->next;
 		i++;
 	}
 
