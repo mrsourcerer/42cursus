7120cab6078e90468151098583d1636bf1bbe1f9
sourcerer <mr.sourcerer@gmail.com>
2023-03-23 07:23:19 +0100

    2023.03.23 07:22h home, push_swap strugling with 100 elements

--- push_swap_no_git/ft_sort_mid.c
+++ push_swap_no_git/ft_sort_mid.c
@@ -6,13 +6,13 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 07:25:49 by danlopez          #+#    #+#             */
-/*   Updated: 2023/03/22 20:52:59 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/03/23 07:21:23 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_swap_what(t_list **a, t_list **b)
+int	ft_swap_what(t_list **a, t_list **b, int a_ok, int b_ok)
 {
 	int	sw_a;
 	int	sw_b;
@@ -25,38 +25,92 @@ int	ft_swap_what(t_list **a, t_list **b)
 	if (ft_lstsize(*b) > 1)
 		if (ft_content(*b) < ft_content((*b)->next))
 			sw_b = 1;
-	if (sw_a && sw_b)
+	if (sw_a && sw_b && !a_ok && !b_ok)
 		ft_swap(a, b, 's');
-	else if (sw_a)
+	else if (sw_a && !a_ok)
 		ft_swap(a, b, 'a');
-	else if (sw_b)
+	else if (sw_b && !b_ok)
 		ft_swap(a, b, 'b');
 	return (sw_a + sw_b);
 }
 
-int ft_rotate_what(t_list **a, t_list **b)
+int	ft_rotate_what(t_list **a, t_list **b)
 {
-	ft_rotate(a, b);
+	int	sw_a;
+	int	sw_b;
+
+	sw_a = 0;
+	sw_b = 0;
+	if (ft_lstsize(*a) > 1)
+		if (ft_content(*a) > ft_content (ft_lstlast(*a)))
+			sw_a = 1;
+	if (ft_lstsize(*b) > 1)
+		if (ft_content(*b) < ft_content (ft_lstlast(*b)))
+			sw_b = 1;
+	if (sw_a && sw_b)
+		ft_rotate(a, b, 'r');
+	else if (sw_a)
+		ft_rotate(a, b, 'a');
+	else if (sw_b)
+		ft_rotate(a, b, 'b');
+	return (sw_a + sw_b);
 }
 
 int	ft_revrotate_what(t_list **a, t_list **b)
 {
-	ft_revrotate(a, b);
+	int	sw_a;
+	int	sw_b;
+
+	sw_a = 0;
+	sw_b = 0;
+	if (ft_lstsize(*a) > 1)
+		if (ft_content(*a) < ft_content (ft_lstlast(*a)))
+			sw_a = 1;
+	if (ft_lstsize(*b) > 1)
+		if (ft_content(*b) > ft_content (ft_lstlast(*b)))
+			sw_b = 1;
+	if (sw_a && sw_b)
+		ft_revrotate(a, b, 'r');
+	else if (sw_a)
+		ft_revrotate(a, b, 'a');
+	else if (sw_b)
+		ft_revrotate(a, b, 'b');
+	return (sw_a + sw_b);
 }
 
 int	ft_sort_mid(t_list **a, t_list **b)
 {
+	int	a_ok;
+	int	b_ok;
+
+	a_ok = 0;
+	b_ok = 0;
+	while (ft_lstsize(*a) > ft_lstsize(*b))
+	{
+		if (ft_content(*a) < 7)
+			ft_push(a, b, 'b');
+		ft_rotate(a, b, 'a');
+	}
 	while (!ft_sorted_int(*a))
 	{
-		ft_swap_what(a, b);
-		if (ft_sorted_int(*a))
-			return (1);
+		ft_swap_what(a, b, a_ok, b_ok);
+		//if (ft_sorted_int(*a))
+		//	break ;
 		ft_rotate_what(a, b);
-		if (ft_sorted_int(*a))
-			return (2);
+		//if (ft_sorted_int(*a))
+		//	break ;
+		//ft_swap_what(a, b);
+		//if (ft_sorted_int(*a))
+		//	break ;
 		ft_revrotate_what(a, b);
 		if (ft_sorted_int(*a))
-			return (3);
+			break ;
+		//ft_printf("stack a\n");
+		//ft_lstprintf(*a, 'i');
+	}
+	while (ft_lstsize(*b) > 0)
+	{
+		ft_push(a, b, 'a');
 	}
 	return (0);
 }
--- push_swap_no_git/push_swap.h
+++ push_swap_no_git/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 08:55:14 by danlopez          #+#    #+#             */
-/*   Updated: 2023/03/22 20:54:16 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/03/23 07:17:25 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int		ft_sort_min_four(t_list **a, t_list **b);
 int		ft_sort_min_five(t_list **a, t_list **b);
 
 int		ft_sort_mid(t_list **a, t_list **b);
-int		ft_swap_what(t_list **a, t_list **b);
-int		ft_rotate_what(t_list **a, t_list **b);
-int		ft_revrotate_what(t_list **a, t_list **b);
+int		ft_swap_what(t_list **a, t_list **b, int a_ok, int b_ok);
+int		ft_rotate_what(t_list **a, t_list **b, int a_ok, int b_ok);
+int		ft_revrotate_what(t_list **a, t_list **b, int a_ok, int b_ok);
 
 int		ft_sorted_int(t_list *a);
 int		ft_duplicated(int *a, int len);
