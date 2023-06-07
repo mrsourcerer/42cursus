9aae50ec4ca42e76d6c8e4c2f894f21e426f8979
sourcerer <mr.sourcerer@gmail.com>
2023-06-07 07:15:06 +0200

    2023.06.07 07:14h home, minitalk bonus... perhaps giving up

--- minitalk/client_bonus.c
+++ minitalk/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 12:33:14 by danlopez          #+#    #+#             */
-/*   Updated: 2023/06/06 06:41:26 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/06/07 07:13:48 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,30 @@ int	ft_no_ok(char *str_num)
 	return (0);
 }
 
+void	ft_send_client_pid(pid_t pid, pid_t client_pid)
+{
+	char	*str_pid;
+
+	str_pid = ft_itoa(client_pid);
+	while (*str_pid)
+	{
+		ft_send_char(pid, *str_pid);
+		str_pid++;
+	}
+	ft_send_char(pid, '\0');
+}
+
 int	main(int argc, char *argv[])
 {
 	pid_t		pid;
+	pid_t		client_pid;
 	sigset_t	signal_set;
 
 	if (argc != 3)
 		return (-1);
 	if (ft_no_ok(argv[1]))
 		return (-1);
+	client_pid = getpid();
 	pid = ft_atoi(argv[1]);
 	g_str = argv[2];
 	ft_printf("Num: %i\n", pid);
@@ -70,6 +85,7 @@ int	main(int argc, char *argv[])
 	sigemptyset(&signal_set);
 	sigaddset(&signal_set, SIGUSR1);
 	sigaddset(&signal_set, SIGUSR2);
+	ft_send_client_pid(pid, client_pid);
 	ft_send_str(pid);
 	return (0);
 }
