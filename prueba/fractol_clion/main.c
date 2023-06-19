fdb1c341b639b4c88b41c5811f6c79f327c66565
sourcerer <mr.sourcerer@gmail.com>
2023-06-19 07:23:56 +0200

    2023.06.19 07:23h home. Testing minilibx

--- prueba/fractol_clion/main.c
+++ prueba/fractol_clion/main.c
@@ -6,12 +6,13 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 19:25:21 by danlopez          #+#    #+#             */
-/*   Updated: 2023/06/17 13:01:18 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/06/19 07:17:56 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minilibx_mms_20191025_beta/mlx.h"
 #include <stdio.h>
+#include <stdlib.h>  
 
 typedef struct	s_vars {
 	void	*mlx;
@@ -21,7 +22,14 @@ typedef struct	s_vars {
 int	close(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
-	printf("llego\n");
+	exit (0);
+}
+
+int key_press(int key, void *param)
+{
+	printf("key_press");
+	(void)key;
+	close(param);
 	return (0);
 }
 
@@ -30,7 +38,8 @@ int	main(void)
 	t_vars	vars;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_hook(vars.win, 2, 0, key_press, &vars);
+	mlx_hook(vars.win, 17, 0, close, &vars);
 	mlx_loop(vars.mlx);
 }
