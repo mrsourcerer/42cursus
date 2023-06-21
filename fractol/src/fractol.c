7c1d12833877844a6ce19132d567d2452aa256aa
dani <danlopez@student.42urduliz.com>
2023-06-21 21:05:20 +0200

    2023.06.21 21:04h 42. Fractol, test finished ready to start

--- fractol/src/fractol.c
+++ fractol/src/fractol.c
@@ -6,9 +6,14 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 07:14:55 by danlopez          #+#    #+#             */
-/*   Updated: 2023/06/21 07:15:41 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/06/21 19:39:54 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
 
+int	main(void)
+{
+	ft_printf("Parece que compila\n");
+	return (0);
+}
