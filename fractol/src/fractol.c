7fa7aa4a7cfa418a63174d2bd0907f887ac7a88c
sourcerer <mr.sourcerer@gmail.com>
2023-06-23 07:22:23 +0200

    2023.06.23 07:21h home. Fractol, checking args

--- fractol/src/fractol.c
+++ fractol/src/fractol.c
@@ -6,14 +6,35 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 07:14:55 by danlopez          #+#    #+#             */
-/*   Updated: 2023/06/21 19:39:54 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/06/23 07:20:58 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
 
-int	main(void)
+int	ft_args_ok(int argc, char *argv[])
 {
-	ft_printf("Parece que compila\n");
+	int	bad_args;
+
+	bad_args = 0;
+	if (argc == 1)
+		bad_args = 1;
+	if (argc == 2)
+		if (ft_strlen(ft_strnstr(argv[1], "mandelbrot", 10)) == 10 || \
+			ft_strlen(ft_strnstr(argv[1], "julia", 5)) == 5)
+			bad_args = 1;
+	if (bad_args)
+	{
+		ft_printf("Usage: fractol mandelbrot or fractol julia\n");
+		return (1);
+	}
+	ft_printf("args ok\n");
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (!ft_args_ok(argc, argv))
+		return (-1);
 	return (0);
 }
