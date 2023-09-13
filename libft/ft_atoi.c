488d078f35e93e937995725b4978afabd6c84310
sourcerer <mr.sourcerer@gmail.com>
2023-09-13 07:27:57 +0200

    2023.09.13 7:27h home, ft_atof not working...

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: danlopez <danlopez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 12:10:18 by danlopez          #+#    #+#             */
-/*   Updated: 2023/06/25 09:50:47 by danlopez         ###   ########.fr       */
+/*   Updated: 2023/09/13 07:23:48 by danlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,22 @@ long	ft_atol(const char *str)
 	return (n * negative);
 }
 
+static float	ft_decimal(const char *str)
+{
+	float	dec;
+	int		i;
+
+	dec = 0;
+	i = 10;
+	while (*str >= '0' && *str <= '9')
+	{
+		dec = dec + (*str - '0') / i;
+		i = i * 10;
+		str++;
+	}
+	return (dec);
+}
+
 double	ft_atof(const char *str)
 {
 	double	n;
@@ -77,6 +93,11 @@ double	ft_atof(const char *str)
 	{
 		n = n * 10 + (*str - '0');
 		str++;
+	}
+	if (*str == '.')
+	{
+		str++;
+		n = n + ft_decimal(str);
 	}
 	return (n * negative);
 }
